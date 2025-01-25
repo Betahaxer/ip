3f77ebe0572f6153ca7e2ed99bccf39e9c49497b
Betahaxer <yuwenpoh@gmail.com>
2025-01-25 10:38:08 +0800

    Update rename, greet and exit

---
+++ src/main/java/Bond.java
@@ -0,0 +1,8 @@
+public class Bond {
+    public static void main(String[] args) {
+        String greet = "Hello! I'm Bond, a dog that can predict the future.\nWhat can I do for you?\n";
+        String line = "-----------------------------------------------------\n";
+        String bye = "Woof. Hope to see you again soon!\n";
+        System.out.println(line + greet + line + bye + line);
+    }
+}
