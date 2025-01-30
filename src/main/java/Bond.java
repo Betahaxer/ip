39e9bea1ed42bc8fc3ab94229724006c42bf5e83
Betahaxer <yuwenpoh@gmail.com>
2025-01-30 21:43:09 +0800

    Add ability to add task, and view task list

--- src/main/java/Bond.java
+++ src/main/java/Bond.java
@@ -1,31 +1,55 @@
 import java.util.Scanner;
 public class Bond {
-    public static final String RESET = "\u001B[0m";
-    public static final String BOLD = "\u001B[1m";
-    public static final String ITALIC = "\u001B[3m";
-    public static final String UNDERLINE = "\u001B[4m";
-    public static final String RED = "\u001B[31m";
-    public static final String GREEN = "\u001B[32m";
-    public static final String YELLOW = "\u001B[33m";
-    public static final String BLUE = "\u001B[34m";
-    public static final String WHITE = "\033[97m";
-    public static final String HIGHLIGHT = "\u001B[43m";
+    private static final String RESET = "\u001B[0m";
+    private static final String BOLD = "\u001B[1m";
+    private static final String ITALIC = "\u001B[3m";
+    private static final String UNDERLINE = "\u001B[4m";
+    private static final String RED = "\u001B[31m";
+    private static final String GREEN = "\u001B[32m";
+    private static final String YELLOW = "\u001B[33m";
+    private static final String BLUE = "\u001B[34m";
+    private static final String WHITE = "\033[97m";
+    private static final String HIGHLIGHT = "\u001B[43m";
+    private static final String TAB = "    ";
+    private static final String COMMAND = "> ";
+    private static String[] list = new String[100];
+    private static int taskCount = 0;
 
     public static void greet() {
-        String greet = WHITE + "Hello! I'm Bond, a dog that can predict the future.\nWhat can I do for you?\n" + GREEN + "> ";
+        String greet = TAB + WHITE + "Hello! I'm Bond, a dog that can predict the future.\n" + TAB + "What can I do for you?\n" + GREEN + COMMAND;
         System.out.print(greet);
     }
 
     public static void sayBye() {
-        String bye = WHITE + "Woof. Hope to see you again soon!";
+        String bye = TAB + WHITE + "Woof. Hope to see you again soon!";
         System.out.println(bye);
     }
+
+    public static void addToList (String task) {
+        list[taskCount] = task;
+        taskCount += 1;
+        System.out.println(TAB + WHITE + "added: " + task);
+        System.out.print(GREEN + COMMAND);
+    }
+
+    public static void showList () {
+        for (int i = 0; list[i] != null; i++) {
+            System.out.printf(TAB + WHITE + "%d" + ". " + list[i] + "\n", i + 1);
+        }
+        System.out.print(GREEN + COMMAND);
+    }
+
     public static void main(String[] args) {
         greet();
         Scanner in = new Scanner(System.in);
         String instruction;
         while (!(instruction = in.nextLine()).equals("bye")) {
-            System.out.print(WHITE + BOLD + instruction + RESET + GREEN + "\n> ");
+            if (instruction.equals("list")) {
+                showList();
+            }
+            else {
+                addToList(instruction);
+            }
         }
         sayBye();
     }
